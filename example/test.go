88853e1a65eb0c0105e72650dc5fe84abb759f49
gcoka <y.okamoto@gemcook.com>
2018-07-04 03:00:11 +0900

    [update] example server impl graceful shutdown

--- example/test.go
+++ example/test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,10 +15,44 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf8")
 	w.WriteHeader(200)
 
+	fmt.Println("Access from", r.UserAgent())
+
 	fmt.Fprintf(w, "hello %v", "world")
 }
 
-func main() {
+func startHTTPServer() *http.Server {
+	srv := &http.Server{Addr: ":8080"}
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Httpserver: ListenAndServe() error: %s", err)
+		}
+	}()
+	return srv
+}
+
+func main() {
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh,
+		os.Interrupt,
+		os.Kill,
+	)
+
+	srv := startHTTPServer()
+	fmt.Println("test server started on localhost:8080")
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, os.Kill)
+	sig := <-quit
+
+	fmt.Println("Shutdown Server with Signal", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalln("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
